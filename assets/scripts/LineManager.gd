2df572d8b1003d717827ff7693c7e3ea4b5a0ea5
Tekknobot <thrillatron@hotmail.com>
2024-05-20 13:53:31 -0400

    Add sfx.

--- assets/scripts/LineManager.gd
+++ assets/scripts/LineManager.gd
@@ -24,7 +24,7 @@ func ready():
 
 func _process(_delta):
 	pass
-		
+
 func _input(event):		
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:	
@@ -44,8 +44,23 @@ func _input(event):
 					var tile_pos = Map.local_to_map(mouse_position)
 					var tile_center_pos = Map.map_to_local(tile_pos) + Vector2(0,0) / 2
 					_point2 = mouse_position
-					_cubic_bezier(line_2d, point1, Vector2(0, -250), Vector2(0, -250), _point2, 1)
-						
+					$"../AudioStreamPlayer2D".stream = $"../AudioStreamPlayer2D".map_sfx[0]
+					$"../AudioStreamPlayer2D".play()						
+					await _cubic_bezier(line_2d, point1, Vector2(0, -250), Vector2(0, -250), _point2, 1)
+
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_1:
+			for i in 1:
+				var coord_A = get_node("/root/Node2D").structures[rng.randi_range(0, get_node("/root/Node2D").structures.size()-1)].coord
+				var coord_B = get_node("/root/Node2D").structures[rng.randi_range(0, get_node("/root/Node2D").structures.size()-1)].coord
+				var tile_pos = Map.map_to_local(coord_A) + Vector2(0,0) / 2					
+				var tile_pos2 = Map.map_to_local(coord_B) + Vector2(0,0) / 2	
+				$"../AudioStreamPlayer2D".stream = $"../AudioStreamPlayer2D".map_sfx[0]
+				$"../AudioStreamPlayer2D".play()						
+				_cubic_bezier(line_2d, tile_pos, Vector2(0, -250), Vector2(0, -250), tile_pos2, 1)	
+				#var dup = self.duplicate()
+				#self.get_parent().add_child(dup)							
+								
 func _cubic_bezier(line_2d: Line2D, p0: Vector2, p1: Vector2, p2: Vector2, p3: Vector2, t: float):
 	var line_inst = line2D.instantiate()
 	add_child(line_inst)
@@ -57,18 +72,34 @@ func _cubic_bezier(line_2d: Line2D, p0: Vector2, p1: Vector2, p2: Vector2, p3: V
 	for i in points.size():
 		line_inst.add_point(points[i])
 		await get_tree().create_timer(0).timeout
-		
+
 	line_2d.clear_points()	
-	var tile_position = Map.local_to_map(points[points.size()-1])
-	var tile_pos = Map.map_to_local(tile_position) + Vector2(0,0) / 2
+	var point_position = Map.local_to_map(points[points.size()-1])
+	var point_pos = Map.map_to_local(point_position) + Vector2(0,0) / 2
 	for i in get_node("/root/Node2D").structures.size():
-		if tile_position == get_node("/root/Node2D").structures[i].coord:
+		if point_position == get_node("/root/Node2D").structures[i].coord:
 			var tween: Tween = create_tween()
 			#tween.tween_property(get_node("/root/Node2D").structures[i], "modulate:v", 1, 0.1).from(5)	
 			get_node("/root/Node2D").structures[i].get_child(0).play("demolished")	
 			
 			var explosion_instance = explosion.instantiate()
 			get_parent().add_child(explosion_instance)
-			explosion_instance.position = tile_pos
-			explosion_instance.z_index = (tile_pos.x + tile_pos.y) + 4		
-			line_inst.hide()	
+			var explosion_pos = Map.map_to_local(get_node("/root/Node2D").structures[i].coord) + Vector2(0,0) / 2
+			explosion_instance.position = explosion_pos
+			explosion_instance.z_index = (point_pos.x + point_pos.y) + 4
+				
+			$"../AudioStreamPlayer2D".stream = $"../AudioStreamPlayer2D".map_sfx[1]
+			$"../AudioStreamPlayer2D".play()	
+				
+	for i in 4:
+		line_inst.set_antialiased(false)
+		line_inst.set_width(1)	
+		line_inst.set_default_color(Color.WHITE_SMOKE)	
+		await get_tree().create_timer(0.05).timeout
+		line_inst.set_width(4)
+		line_inst.set_default_color(Color.GRAY)
+		await get_tree().create_timer(0.05).timeout
+	
+	line_inst.hide()
+			
+	await get_tree().create_timer(0.3).timeout		
